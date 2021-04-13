2664b047d751669d3a1ccf49a23c2e1710c3de1c
Nobuo Imai <8196611+n-imai@users.noreply.github.com>
2021-04-13 21:50:06 +0900

    add components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
   return (
     <div className="App">
       <h2>Let's get started!</h2>
-        <p>This is also visible</p>
+        <ExpenseItem></ExpenseItem>
     </div>
   );
 }
